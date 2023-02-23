fde139ca5e248895bd85bd9db1af1ab8addded9d
Calypso Riebe <100364919+calypsoriebe@users.noreply.github.com>
2023-02-23 14:44:08 -0500

    laid out skeleton for class methods

--- budgetTracker.js
+++ budgetTracker.js
@@ -1,3 +1,21 @@
 export default class BudgetTracker {
   constructor(querySelector) {}
+  //return table html
+  static html() {}
+  //html for single row entries
+  static entryHtml() {}
+  //initial load data
+  load() {}
+  //will work out total of table
+  updateSumm() {}
+  //saves to storage for when refreshing page
+  save() {}
+  //for adding new entry
+  addEntry(entry = {}) {}
+  //get helper to return all active entries in table
+  getEntries() {}
+  //button for making new entry
+  newEntryBtn() {}
+  //for deleting entry
+  deleteEntryBtn() {}
 }
