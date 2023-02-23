fe3bba1ea18727571c6369014933b7a4a486a92f
Calypso Riebe <100364919+calypsoriebe@users.noreply.github.com>
2023-02-23 14:58:28 -0500

    added html into static js methods

--- budgetTracker.js
+++ budgetTracker.js
@@ -1,9 +1,72 @@
 export default class BudgetTracker {
-  constructor(querySelector) {}
+  constructor(querySelector) {
+    this.root = document.querySelector(querySelector);
+    //makes innerHTML the html wihtin the static method
+    this.root.innerHTML = BudgetTracker.html();
+    //selecting the new entry button and adding listner for when clicking
+    this.root.querySelector(".newEntry").addEventListener("click", () => {
+      this.newEntryBtn;
+    });
+    //will load data from storage
+    this.load();
+  }
   //return table html
-  static html() {}
+  static html() {
+    return `
+        <table class="budgetTracker">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody class="entries"></tbody>
+            <tbody>
+                <tr>
+                    <td colspan="5" class="controls">
+                        <button type="button" class="newEntry">New Entry</button>
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="5" class="summary">
+                        <strong>Total:</strong>
+                        <span class="total">$0.00</span>
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
+      `;
+  }
   //html for single row entries
-  static entryHtml() {}
+  static entryHtml() {
+    return ` 
+        <tr>
+            <td>
+                <input type="date" class="input dateInput" />
+            </td>
+            <td>
+                <input type="text" placeholder="I.e Wages, bills, etc" class="input descriptInput"/>
+            </td>
+            <td>
+                <select class="input selectInput">
+                    <option value="income">Income</option>
+                    <option value="expense">Expense</option>
+                </select>
+            </td>
+            <td>
+                <input type="number" class="input amountInput" placeholder="0.00"/>
+            </td>
+            <td>
+                <button type="button" class="deleteEntry">&#10005;</button>
+            </td>
+        </tr>
+    `;
+  }
   //initial load data
   load() {}
   //will work out total of table
